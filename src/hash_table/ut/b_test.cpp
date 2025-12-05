3b1b72d4657b1a2194bf6b05710cacde5f33a923
German Perov <ggmusya@gmail.com>
2025-12-05 17:41:44 +0300

    tmp

--- src/hash_table/ut/b_test.cpp
+++ src/hash_table/ut/b_test.cpp
@@ -1,21 +1,8 @@
-#include "hash_table/b.h"
+#include "gtest/gtest.h"
 
 #include <cstdlib>
 #include <iostream>
 
-void test1() {
-  int a = 5;
-  int b = 6;
-
-  int result = foo(a, b);
-  int expected = a + b;
-
-  if (result != expected + 1) {
-    std::cerr << "AAAA" << std::endl;
-    exit(1);
-  }
-}
-
-int main() {
-    test1();
+TEST(A, B) {
+  ASSERT_EQ(2 + 2, 5);
 }
